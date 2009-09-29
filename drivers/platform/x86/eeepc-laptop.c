0ea02d0897093b3dd2ef3ee90c02b723156e4aeb
Corentin Chary <corentincj@iksaif.net>
2009-09-29 16:15:50 +0200

    eeepc-laptop: whitespace for checkpatch.pl
    checkpatch doesn't like tab+space for a return statement.
    
    WARNING: suspect code indent for conditional statements (8, 17)
    +	if (!device)
    +		 return -EINVAL;
    
    Signed-off-by: Len Brown <len.brown@intel.com>
    Signed-off-by: Corentin Chary <corentincj@iksaif.net>

--- drivers/platform/x86/eeepc-laptop.c
+++ drivers/platform/x86/eeepc-laptop.c
@@ -1208,7 +1208,7 @@ static int eeepc_hotk_add(struct acpi_device *device)
 	int result;
 
 	if (!device)
-		 return -EINVAL;
+		return -EINVAL;
 	pr_notice(EEEPC_HOTK_NAME "\n");
 	ehotk = kzalloc(sizeof(struct eeepc_hotk), GFP_KERNEL);
 	if (!ehotk)
@@ -1297,7 +1297,7 @@ static int eeepc_hotk_remove(struct acpi_device *device, int type)
 	acpi_status status;
 
 	if (!device || !acpi_driver_data(device))
-		 return -EINVAL;
+		return -EINVAL;
 
 	eeepc_backlight_exit();
 	eeepc_rfkill_exit();
