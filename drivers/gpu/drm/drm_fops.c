f964208bc79fa12a294a968aa85141eb48e7f7f1
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-02-09 10:43:27 +0800

    drm: disable GPU authentication

--- drivers/gpu/drm/drm_fops.c
+++ drivers/gpu/drm/drm_fops.c
@@ -252,7 +252,7 @@ static int drm_open_helper(struct inode *inode, struct file *filp,
 	priv->minor = idr_find(&drm_minors_idr, minor_id);
 	priv->ioctl_count = 0;
 	/* for compatibility root is always authenticated */
-	priv->authenticated = capable(CAP_SYS_ADMIN);
+	priv->authenticated = 1;
 	priv->lock_count = 0;
 
 	INIT_LIST_HEAD(&priv->lhead);
