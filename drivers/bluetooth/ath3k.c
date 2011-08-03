50a5d896a1bbed0c5e79fb28725cb0bceb9f19b3
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-08-03 16:43:22 +0800

    bluetooth: ath3k: use new firmware ath3k-2.fw

--- drivers/bluetooth/ath3k.c
+++ drivers/bluetooth/ath3k.c
@@ -393,7 +393,7 @@ static int ath3k_probe(struct usb_interface *intf,
 		return 0;
 	}
 
-	if (request_firmware(&firmware, "ath3k-1.fw", &udev->dev) < 0) {
+	if (request_firmware(&firmware, "ath3k-2.fw", &udev->dev) < 0) {
 		BT_ERR("Error loading firmware");
 		return -EIO;
 	}
@@ -434,4 +434,4 @@ MODULE_AUTHOR("Atheros Communications");
 MODULE_DESCRIPTION("Atheros AR30xx firmware driver");
 MODULE_VERSION(VERSION);
 MODULE_LICENSE("GPL");
-MODULE_FIRMWARE("ath3k-1.fw");
+MODULE_FIRMWARE("ath3k-2.fw");
