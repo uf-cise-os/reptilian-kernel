76370c9a7be56495aec11c34251b6714d862cd36
Stefan Seidel <android@stefanseidel.info>
2012-02-25 18:56:55 +0100

    send current tablet mode switch state on resume so that applications will know if it has changed during suspend

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -3769,6 +3769,7 @@ static void hotkey_resume(void)
 		       "firmware interface\n");
 
 	tpacpi_send_radiosw_update();
+	tpacpi_input_send_tabletsw();
 	hotkey_tablet_mode_notify_change();
 	hotkey_wakeup_reason_notify_change();
 	hotkey_wakeup_hotunplug_complete_notify_change();
