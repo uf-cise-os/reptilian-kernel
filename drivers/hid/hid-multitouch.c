d07e9e8d5a5f7045371412e8782ad7a48883ce18
Chih-Wei Huang <cwhuang@linux.org.tw>
2012-07-02 16:02:13 +0800

    HID: hid-multitouch: add one new multitouch device's VID/PID
    This patch adds support for the Atmel panel.

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -666,6 +666,11 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_ACTIONSTAR,
 			USB_DEVICE_ID_ACTIONSTAR_1011) },
 
+	/* Atmel touch panels */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_ATMEL,
+			USB_DEVICE_ID_ATMEL_MAXTOUCH) },
+
 	/* Cando panels */
 	{ .driver_data = MT_CLS_DUAL_INRANGE_CONTACTNUMBER,
 		HID_USB_DEVICE(USB_VENDOR_ID_CANDO,
