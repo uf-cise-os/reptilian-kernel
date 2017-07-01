8cc0c33e4f357fb7d0f6901b7206b069a76f531b
Mauro Rossi <issor.oruam@gmail.com>
2017-07-02 01:40:32 +0200

    net: wireless: broadcom: wl: fix kernel >= 4.8 panic
    linux-recent.patch was ok up till kernel 4.7
    with kernel 4.8 (and 4.9) new changes are critical to avoid crash
    
    build.mk is modified to apply incremental patch linux-48.patch
    copied from https://aur.archlinux.org/cgit/aur.git/tree/?h=broadcom-wl
    
    Reference: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=839629
    
    v2: cat all patches to apply them once (cwhuang)

--- drivers/net/wireless/broadcom/wl/build.mk
+++ drivers/net/wireless/broadcom/wl/build.mk
@@ -3,14 +3,19 @@
 WL_ENABLED := $(if $(wildcard $(WL_PATH)),$(shell grep ^CONFIG_WL=[my] $(KERNEL_CONFIG_FILE)))
 WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_271.tar.gz
 WL_LIB := $(WL_PATH)/lib$(if $(filter x86,$(TARGET_KERNEL_ARCH)),32,64)
+
+WL_PATCHES := \
+	wl.patch \
+	linux-recent.patch \
+	linux-48.patch \
+
 $(WL_SRC):
 	@echo Downloading $(@F)...
 	$(hide) curl -k https://docs.broadcom.com/docs-and-downloads/docs/linux_sta/$(@F) > $@
 
-$(WL_LIB): $(WL_SRC) $(wildcard $(WL_PATH)/*.patch)
+$(WL_LIB): $(WL_SRC) $(addprefix $(WL_PATH)/,$(WL_PATCHES))
 	$(hide) tar zxf $< -C $(@D) --overwrite -m && \
 		rm -rf $@ && mv $(@D)/lib $@ && touch $@ && \
-		patch -p1 -d $(@D) -i wl.patch && \
-		patch -p1 -d $(@D) -i linux-recent.patch
+		cat $(filter %.patch,$^) | patch -p1 -d $(@D)
 
 $(INSTALLED_KERNEL_TARGET): $(if $(WL_ENABLED),$(WL_LIB))
